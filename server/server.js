6ab5925b20a3d37e6db066dcfa74a2380ffc9cb6
Gabriel Wilkins <wilkigab001@gmail.com>
2022-09-25 23:37:47 -0600

    I added a few extra details to make sure this works

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(port, () => {
+    console.log('listening on port ' + port)
+})
