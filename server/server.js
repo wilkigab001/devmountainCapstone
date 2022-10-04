6239a545fb62bbad6f09e18a34939c8abd8e2d74
Gabriel Wilkins <wilkigab001@gmail.com>
2022-10-04 15:45:54 -0600

    Heroku please worku

--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const path = require('path')
 
 const app = express()
 
 //require('dotenv').config()
 
-//const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/', express.static(path.join(__dirname, '../client/trails.html')))
+app.use(express.static(path.join(__dirname, '../client')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+app.get('/js/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+})
+
+app.get('/css/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.css'))
+})
+
 
 const controller = require('./controller')
 const {showTrails,
@@ -28,6 +46,6 @@ app.put('/changeTrailName/:id', changeTrailName)
 //app.put('/changeTrailName/:id', changeTrailName)
 
 
-app.listen(4567, () => {
-    console.log('Justice league just got their butts kicked on port ' + 4567)
+app.listen(port, () => {
+    console.log('Justice league just got their butts kicked on port ' + port)
 })
